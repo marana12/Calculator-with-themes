8457735832a76d9ec039021a83d1067f9bf93497
Julio <mizrahijulio@gmail.com>
2021-07-30 17:58:58 +0300

    first-commit

---
+++ JS/js.js
@@ -0,0 +1,104 @@
+var number1=0;
+var number2=0;
+var functionCalc="";
+var result='';
+
+$('.toggle-ball').click(function(){
+    $('.toggle-ball').removeClass('active');
+    $(this).addClass('active');
+    $('body').removeClass().addClass($(this).attr('targ'));
+})
+
+$('.number').click(function(){
+    let number=$(this).attr('target');
+
+    $("#calcInput").val( function(index,value){
+        if(number===','){
+           if(value.indexOf(number)>0)
+           return value;
+        }
+        return value+number
+    });
+});
+
+
+$("#delete").click(function(){
+    $("#calcInput").val(
+        function(index, value){
+            return value.substr(0, value.length - 1);
+    })
+})
+$("#reset").click(function(){
+    $("#calcInput").val('');
+    reset();
+
+});
+
+$('.calcs').click(function(){
+    functionCalc=$(this).attr('target');
+    let input=$("#calcInput");
+
+    if(input.val().length!=0){
+        if(result.length == 0){
+            result=input.val().replace(',','.')
+            number1=parseFloat(result);
+        }else{
+            number1=parseFloat(input.val().replace(',','.'));
+            calc();
+        }
+        input.val('');
+
+    }
+});
+
+$("#equals").click(function(){
+    let input=$("#calcInput");
+
+    number1=parseFloat(input.val().replace(',','.'));
+    calc();
+    reset();
+});
+
+function calc(){
+    let input=$("#calcInput");
+    let resultFloat=parseFloat(result.replace(',','.'));
+
+    switch(functionCalc){
+        case '+':
+            console.log(number1+ ' + '+ resultFloat)
+
+            resultFloat = number1 + resultFloat
+
+            break;
+        case '-':
+            console.log(number1+ ' - '+ resultFloat)
+
+            resultFloat = number1 - resultFloat;
+
+            break;
+        case '/':
+            console.log(number1+ ' / '+ resultFloat)
+
+            resultFloat= number1 / resultFloat;
+            break;
+        case 'x':
+            console.log(number1+ ' * '+ resultFloat)
+
+            resultFloat = number1 * resultFloat;
+            break;
+        default:
+            result="";
+            break;
+    }
+
+    result=resultFloat.toString().replace('.',',');
+    
+    input.val(result);
+}
+
+function reset(){
+    number1=0;
+    number2=0;
+    functionCalc='';
+    result='';
+}
---
+++ README.md
@@ -0,0 +1,59 @@
+# Frontend Mentor - Calculator app solution
+
+This is a solution to the [Calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See the size of the elements adjust based on their device's screen size
+- Perform mathmatical operations like addition, subtraction, multiplication, and division
+- Adjust the color theme based on their preference
+
+
+### Screenshot
+Desktop
+- Theme 1
+![](./images/theme1Desktop.png)
+- Theme 2
+![](./images/theme2Desktop.png)
+- Theme 3
+![](./images/theme3Desktop.png)
+
+Mobile
+- Theme 1
+![](./images/theme1Mobile.png)
+- Theme 2
+![](./images/theme2Mobile.png)
+- Theme 3
+![](./images/theme3Mobile.png)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- JQuery
+- CSS Grid
+- Mobile-first workflow
+
+
+## Author
+
+- Frontend Mentor - [@marana12](https://www.frontendmentor.io/profile/marana12)
+
